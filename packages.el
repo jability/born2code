0ba3cf110910bbc3177d835a89344f78da265339
Julien LINDEN <jlinden@student.42.fr>
2016-12-20 10:41:09 +0100

    Remove packages.el default comments

--- packages.el
+++ packages.el
@@ -1,34 +1,3 @@
-;;; packages.el --- born2code layer packages file for Spacemacs.
-;;
-;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
-;;
-;; Author: jlinden <jlinden@kb-firmware>
-;; URL: https://github.com/syl20bnr/spacemacs
-;;
-;; This file is not part of GNU Emacs.
-;;
-;;; License: GPLv3
-
-;;; Commentary:
-
-;; See the Spacemacs documentation and FAQs for instructions on how to implement
-;; a new layer:
-;;
-;;   SPC h SPC layers RET
-;;
-;;
-;; Briefly, each package to be installed or configured by this layer should be
-;; added to `born2code-packages'. Then, for each package PACKAGE:
-;;
-;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
-;;   function `born2code/init-PACKAGE' to load and initialize the package.
-
-;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
-;;   define the functions `born2code/pre-init-PACKAGE' and/or
-;;   `born2code/post-init-PACKAGE' to customize the package as it is loaded.
-
-;;; Code:
-
 (defconst born2code-packages
   '(
 	flycheck
@@ -85,5 +54,3 @@
 ;;         :modes (c-mode))
 ;;       (add-to-list 'flycheck-checkers 'norminette)
 ;; 	  ))
-
-;;; packages.el ends here
