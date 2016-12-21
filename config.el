9fca57c649ed92019e1480bfe4b7a7c35055bc8c
Julien LINDEN <jlinden@student.42.fr>
2016-12-21 20:46:57 +0100

    experiment with comments part1

--- config.el
+++ config.el
@@ -1,5 +1,13 @@
 ;; 42 C style
 (setq-default c-default-style "linux"
 			  c-basic-offset 4
+			  c-block-comment-prefix "**"
 			  tab-width 4
 			  indent-tabs-mode t)
+
+(defun close-comm ()
+  (setq-default comment-multi-line t)
+  (define-key evil-insert-state-map (kbd "RET") 'c-context-line-break)
+  (add-to-list 'c-cleanup-list 'comment-close-slash))
+
+(add-hook 'c-mode-hook 'close-comm)
