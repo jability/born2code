66b94f131615dd14e59e9de20307515725c816d5
Julien LINDEN <jlinden@student.42.fr>
2017-01-05 21:11:17 +0100

    Add cscope back with correct bindings

--- config.el
+++ config.el
@@ -2,11 +2,11 @@
 (setq-default c-default-style "linux"
 			  c-basic-offset 4
 			  c-block-comment-prefix "**"
+			  comment-multi-line t
 			  tab-width 4
 			  indent-tabs-mode t)
 
 (defun close-comm ()
-  (setq-default comment-multi-line t)
   (define-key evil-insert-state-map (kbd "RET") 'c-context-line-break)
   (add-to-list 'c-cleanup-list 'comment-close-slash))
 
--- packages.el
+++ packages.el
@@ -1,6 +1,8 @@
 (defconst born2code-packages
   '(
 	flycheck
+	(helm-cscope :toggle (configuration-layer/package-usedp 'helm))
+	xcscope
 	(list :location local)
 	(string :location local)
 	(comments :location local)
@@ -38,3 +40,50 @@
         :modes (c-mode))
       (add-to-list 'flycheck-checkers 'norminette)
 	  ))
+
+(defun born2code/init-xcscope ()
+  (use-package xcscope
+    :commands (cscope-index-files cscope/run-pycscope)
+    :init
+    (progn
+      ;; for python projects, we don't want xcscope to rebuild the databse,
+      ;; because it uses cscope instead of pycscope
+      (setq cscope-option-do-not-update-database t
+            cscope-display-cscope-buffer nil)
+
+	  (spacemacs/declare-prefix "mc" "c-scope")
+	  (dolist (mode '(c-mode c++-mode))
+		(spacemacs/set-leader-keys-for-major-mode mode "ci" 'cscope-index-files))
+      (defun cscope//safe-project-root ()
+        "Return project's root, or nil if not in a project."
+        (and (fboundp 'projectile-project-root)
+             (projectile-project-p)
+             (projectile-project-root)))
+
+      (defun cscope/run-pycscope (directory)
+        (interactive (list (file-name-as-directory
+                            (read-directory-name "Run pycscope in directory: "
+                                                 (cscope//safe-project-root)))))
+        (let ((default-directory directory))
+          (shell-command
+           (format "pycscope -R -f '%s'"
+                   (expand-file-name "cscope.out" directory))))))))
+
+(defun born2code/init-helm-cscope ()
+  (use-package helm-cscope
+    :defer t
+    :init
+    (defun spacemacs/setup-helm-cscope (mode)
+      "Setup `helm-cscope' for MODE"
+      (spacemacs/set-leader-keys-for-major-mode mode
+        "cc" 'helm-cscope-find-called-function
+        "cC" 'helm-cscope-find-calling-this-funtcion
+        "cd" 'helm-cscope-find-global-definition
+        "ce" 'helm-cscope-find-egrep-pattern
+        "cf" 'helm-cscope-find-this-file
+        "cF" 'helm-cscope-find-files-including-file
+        "cr" 'helm-cscope-find-this-symbol
+        "cx" 'helm-cscope-find-this-text-string))
+    :config
+    (defadvice helm-cscope-find-this-symbol (before cscope/goto activate)
+(evil--jumps-push))))
